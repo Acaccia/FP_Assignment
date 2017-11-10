9eddf59fb26baf35d0f4bdfc86f73065d9c2cc3e
acaccia <acaccia@ulb.ac.be>
2017-11-10 16:50:21 +0100

    [imp] Desert as a random tile generator

--- src/Data/Desert.hs
+++ src/Data/Desert.hs
@@ -1,7 +1,25 @@
+{-# LANGUAGE MultiWayIf #-}
 module Desert where
 
 import System.Random
 import Control.Monad.State
+import Data.Internal.List2D
 
 data Tile = Sand Bool | Water | Lava | Portal deriving Show
 
+type Desert = List2D Tile
+
+makeDesert :: StdGen -> Desert
+makeDesert g = undefined
+
+randomTile :: Double -> Double -> Double -> Double -> State StdGen Tile
+randomTile tll wll pll lll = do
+  (r, g) <- random <$> get
+  put g
+  if | r < wll -> pure Water
+     | r < pll -> pure Portal
+     | r < lll -> pure Lava
+     | otherwise -> do
+        (r, g') <- random <$> get
+        put g'
+        pure $ Sand (r < tll)
--- src/Data/Internal/List2D.hs
+++ src/Data/Internal/List2D.hs
@@ -1,10 +1,12 @@
-module List2D where
+module Data.Internal.List2D where
 
 import Data.Internal.Nat
 
 newtype List2D a = List2D [[a]]
 
 type Index = (Nat, Nat)
+
+-- List2D
 
 instance Functor List2D where
   fmap f (List2D l) = List2D $ (fmap f) <$> l
