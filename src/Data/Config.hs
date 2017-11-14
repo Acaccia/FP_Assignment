42f39af94ebd20d46401ee30b1e78452618882e2
acaccia <acaccia@ulb.ac.be>
2017-11-14 17:00:24 +0100

    [imp] config error messages

--- src/Data/Config.hs
+++ src/Data/Config.hs
@@ -17,26 +17,26 @@ data Config = Config
   , lava2_ll    :: Double
   } deriving (Show)
 
-data ConfigError = OutOfBounds
+data ConfigError = OutOfBounds String String String
                  | PercentageOver100
-                 | NotANumber
+                 | NotANumber String
 
 instance Show ConfigError where
-  show OutOfBounds       = "A value is out of bounds"
-  show PercentageOver100 = "Sum of all percentages is above 100"
-  show NotANumber        = "Input is not a number"
+  show (OutOfBounds q l u) = q ++ ": value is out of bounds [" ++ l ++ " - " ++ u ++ "]"
+  show PercentageOver100   = "Sum of all percentages is above 100"
+  show (NotANumber q)      = q ++ ": input is not a number"
 
 type EitherCE = Either ConfigError
 
-boundCheck :: (Ord a, Show a) => a -> a -> a -> EitherCE a
-boundCheck low up n = if n > low && n < up
-                      then Right n
-                      else Left OutOfBounds
+boundCheck :: (Ord a, Show a) => String -> a -> a -> a -> EitherCE a
+boundCheck q low up n = if n > low && n < up
+                        then Right n
+                        else Left $ OutOfBounds q (show low) (show up)
 
-eitherReadNum :: (Num a, Read a) => String -> EitherCE a
-eitherReadNum s = case reads s of
+eitherReadNum :: (Num a, Read a) => String -> String -> EitherCE a
+eitherReadNum s q = case reads s of
   [(x, "")] -> Right x
-  _         -> Left NotANumber
+  _         -> Left $ NotANumber q
 
 checkPercentage :: EitherCE Int -> EitherCE Int -> Int -> EitherCE Int
 checkPercentage ex ey z = do
@@ -52,18 +52,20 @@ toPercent x = fromIntegral x / 100
 ask :: (Num a, Read a) => String -> IO (EitherCE a)
 ask question = do
   putStr (question ++ ": ")
-  eitherReadNum <$> getLine
+  eitherReadNum question <$> getLine
 
 askConfig :: IO (Either ConfigError Config)
 askConfig = do
   sight <- ask "sight"
   max_water <- ask "max water"
   seed <- fmap mkStdGen <$> (ask "seed")
-  treasure_ll <- (>>= boundCheck 0 100) <$> ask "treasure likelihood"
-  water_ll <- (>>= boundCheck 0 100) <$> ask "water likelihood"
-  portal_ll <- (>>= boundCheck 0 100) <$> ask "portal likelihood"
-  lava1_ll <- (>>= boundCheck 0 100 >=> checkPercentage water_ll portal_ll) <$> ask "lava likelihood"
-  lava2_ll <- (>>= boundCheck 0 100 >=> checkPercentage water_ll portal_ll) <$> ask "lava (adjacent) likelihood"
+  treasure_ll <- (>>= boundCheck "treasure likelihood" 0 100) <$> ask "treasure likelihood"
+  water_ll <- (>>= boundCheck "water likelihood" 0 100) <$> ask "water likelihood"
+  portal_ll <- (>>= boundCheck "portal likelihood" 0 100) <$> ask "portal likelihood"
+  lava1_ll <- (>>= boundCheck "lava likelihood" 0 100 >=> checkPercentage water_ll portal_ll)
+              <$> ask "lava likelihood"
+  lava2_ll <- (>>= boundCheck "lava (adjacent) likelihood" 0 100 >=> checkPercentage water_ll portal_ll)
+              <$> ask "lava (adjacent) likelihood"
   pure $ Config <$> sight <*> max_water <*> seed <*> treasure_ll
     <*> fmap toPercent water_ll <*> fmap toPercent portal_ll
     <*> fmap toPercent lava1_ll <*> fmap toPercent lava2_ll
