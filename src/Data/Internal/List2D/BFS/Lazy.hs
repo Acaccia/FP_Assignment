d948b2c05bef8e4e6ad5cf4defb4bf7337c051cc
acaccia <acaccia@ulb.ac.be>
2017-11-15 19:31:35 +0100

    [imp] bfs make only one indexing

--- src/Data/Internal/List2D/BFS/Lazy.hs
+++ src/Data/Internal/List2D/BFS/Lazy.hs
@@ -10,9 +10,11 @@ type BFSState = Seq (Position, Direction, Nat)
 
 bfsDistance :: Eq a => a -> [a] -> Position -> List2D a -> Maybe Nat
 bfsDistance target avoid pos ls = if ls ! pos == target then Just 0
-        else evalState go (fromList [(p, d, 1) | d <- [minBound..maxBound], let p = move d pos, p /= pos && ls ! p `notElem` avoid])
+        else evalState go (fromList [(p, d, 1) | d <- [minBound..maxBound], let p = move d pos, p /= pos])
   where go :: State BFSState (Maybe Nat)
         go = get >>= \s -> case viewl s of
-                EmptyL -> pure Nothing
-                ((p, prevD, n) :< t) -> if ls ! p == target then pure (Just n)
-                    else put (t >< fromList [(p', opposed d, n+1) | d <- [minBound..maxBound], let p' = move d p, p' /= p && d /= prevD && ls ! p' `notElem` avoid]) >> go
+                EmptyL               -> pure Nothing
+                ((p, prevD, n) :< t) -> let tile = ls ! p in
+                  if tile == target then pure (Just n)
+                    else if tile `elem` avoid then go
+                    else put (t >< fromList [(p', opposed d, n+1) | d <- [minBound..maxBound], let p' = move d p, p' /= p && d /= prevD]) >> go
