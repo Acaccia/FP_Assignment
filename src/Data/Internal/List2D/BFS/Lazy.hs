985d80e2ff76dae9aad45659a17bedd24607eccc
acaccia <acaccia@ulb.ac.be>
2017-11-16 16:05:23 +0100

    [fix] bfs algorithm

--- src/Data/Internal/List2D/BFS/Lazy.hs
+++ src/Data/Internal/List2D/BFS/Lazy.hs
@@ -4,17 +4,21 @@ import Control.Monad.State
 import Data.Internal.Direction
 import Data.Internal.List2D
 import Data.Sequence           (Seq, ViewL (..), fromList, viewl, (><))
+import Debug.Trace
+import qualified Data.Set as S
 
 type Position = (Nat, Nat)
-type BFSState = Seq (Position, Direction, Nat)
+type BFSState = (Seq (Position, Nat), S.Set Position)
 
-bfsDistance :: Eq a => a -> [a] -> Position -> List2D a -> Maybe Nat
+bfsDistance :: (Show a, Eq a) => a -> [a] -> Position -> List2D a -> Maybe Nat
 bfsDistance target avoid pos ls = if ls ! pos == target then Just 0
-        else evalState go (fromList [(p, d, 1) | d <- [minBound..maxBound], let p = move d pos, p /= pos])
+        else evalState go (fromList [(p, 1) | d <- [minBound..maxBound], let p = move d pos, p /= pos], S.empty)
   where go :: State BFSState (Maybe Nat)
-        go = get >>= \s -> case viewl s of
-                EmptyL               -> pure Nothing
-                ((p, prevD, n) :< t) -> let tile = ls ! p in
-                  if tile == target then pure (Just n)
-                    else if tile `elem` avoid then go
-                    else put (t >< fromList [(p', opposed d, n+1) | d <- [minBound..maxBound], let p' = move d p, p' /= p && d /= prevD]) >> go
+        go = do (s, visited) <- get
+                case viewl s of
+                  EmptyL        -> pure Nothing
+                  ((p, n) :< t) -> let tile = ls ! p
+                                       visited' = S.insert p visited
+                       in if tile == target then pure (Just n)
+                          else if tile `elem` avoid then put (t, visited') >> go
+                          else put (t >< fromList [(p', n+1) | d <- [minBound..maxBound], let p' = move d p, p' /= p], visited') >> go
